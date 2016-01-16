2be31a1afb950afe35a2864e726e6451cb900fc6
Maciej Zieliński <zielinski.maciek92@gmail.com>
2016-01-16 16:23:48 +0100

    poprawa Request-a

--- RSA_new/Program.cs
+++ RSA_new/Program.cs
@@ -5,9 +5,9 @@ namespace RSA_new {
     class Program {
         static void Main(string[] args) {
             CGlobalManager _manager = new CGlobalManager();
-            CGlobalManager.LoadLinks(@"e:\Studia\Goścień\DT14_1.75_Tbps\dd.net");
-            CGlobalManager.LoadRoutes(@"e:\Studia\Goścień\DT14_1.75_Tbps\d.pat");
-            CGlobalManager.LoadRequest(@"e:\Studia\Goścień\DT14_1.75_Tbps\51.dem");
+            //CGlobalManager.LoadLinks(@"e:\Studia\Goścień\DT14_1.75_Tbps\dd.net");
+//            CGlobalManager.LoadRoutes(@"e:\Studia\Goścień\DT14_1.75_Tbps\d.pat");
+            CGlobalManager.LoadRequest(@"D:\maciek\Magister\semestr 2\PSK\RSA_any_uni_dane\RSA any uni dane\DT14 1.75 Tbps\51.dem");
             //CRoute newRoute = new CRoute(newList);
             //foreach (var node in newRoute.ReturnPassedNodes()) {
             //    Console.WriteLine(node);
