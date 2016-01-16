03e5c1c44fd1a16ccd43160383b9880340b177ab
Wojciech Grzesiak <wgrzesiak147@gmail.com>
2016-01-16 16:24:54 +0100

    merge

--- RSA_new/Entities/Solution.cs
+++ RSA_new/Entities/Solution.cs
@@ -13,15 +13,23 @@ namespace RSA_new.Entities
         
         public Solution GetRandomSolution(){
 
-            foreach (var request in CGlobalManager.GlobalRequestList)
-            {
-               //List<CRoute> routes = CGlobalManager.GlobalRoutesList
+            foreach (var request in CGlobalManager.GlobalRequestList){
+                List<CRoute> routes =                              //linq that takes all routes for this request
+                    CGlobalManager.GlobalRoutesList.Where(
+                        x => x.NodeBegin == request.StartNode && x.NodeFinish == request.EndNode).ToList();
+               Random rnd = new Random();
+               int random = rnd.Next(routes.Count() - 1); 
+               CRoute randomRoute = routes.ElementAt(random); //we take random route
+               // here we have draw random channels/slots??
+               RoutesCollection.Add(randomRoute);
             }
-            return new Solution();
+            return this;
         }
 
        
        public int GetCost(){
+        if(!RoutesCollection.Any())
+                throw  new Exception("Can't get cost of empty solution!");
         foreach (var route in RoutesCollection)
         {
             cost += route.Distance*route.TakenSlotsCount;
