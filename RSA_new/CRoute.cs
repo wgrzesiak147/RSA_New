fe45fbd182aba6461bc5aee77dec75576ecdf896
vumipl <196174@student.pwr.edu.pl>
2016-01-17 02:08:03 +0100

    Deleted unneccessary constructor

--- RSA_new/CRoute.cs
+++ RSA_new/CRoute.cs
@@ -13,20 +13,6 @@ namespace RSA_new {
         public int TakenSlotsCount { get; set; } = 0;
         public Dictionary<int, int> DemandSlotMapping = null;
 
-        public CRoute(List<CLink> _linkList) {
-            if (_linkList.Count == 0) throw new ArgumentException("Route does not contain any links");
-            //if (!IsContinuous(_linkList)) throw new ArgumentException("Route is not traversable");
-            passedLinks = new List<CLink>(_linkList);
-            NodeBegin = passedLinks[0].NodeA;
-            NodeFinish = passedLinks[passedLinks.Count - 1].NodeB;
-            passedNodes = new List<int>(
-                from cLink in passedLinks
-                select cLink.NodeA);
-            passedNodes.Add(NodeFinish);
-            foreach (var link in passedLinks) {
-                Distance += link.Distance;
-            }
-        }
         public CRoute(int _routeIndex, int _startNode, int _endNode, List<int> _indexes) {
             Index = _routeIndex;
             NodeBegin = _startNode;
@@ -50,7 +36,6 @@ namespace RSA_new {
             //if (!IsContinuous(tmpLinkList)) throw new ArgumentException("Route " + route + " is not traversable");
 
             passedLinks = new List<CLink>();
-
             int tmpNodeA = _startNode;
             for (int i = 0; i < tmpLinkList.Count; i++) {
                 if (i == 0 &&
